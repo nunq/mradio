12b1ba8c8b38b839ac73ef5108d5fbc66ad9ead5
hyphenc <46054695+hyphenc@users.noreply.github.com>
2020-03-15 18:42:23 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const navbar = document.getElementById("navbar");
+const player = document.getElementById("player");
+const loadingindicator = document.getElementById("loading");
+const channellist = document.getElementById("channellist");
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadchannels();
+}, false);
+
+function stop() {
+  player.pause();
+  navbar.style.display = "none";
+}
+
+function playchannel(url, name) {
+  document.getElementById("playertitle").innerHTML = name;
+  navbar.style.display = "flex";
+  player.src = url;
+  player.load();
+  player.play();
+}
+
+function loadchannels() {
+  for (var i=0; i<streams.length; i++) {
+    channellist.insertAdjacentHTML("beforeend", "<div onclick='playchannel(\""+streams[i][1]+"\", \""+streams[i][0]+"\")' class='channel'><img class='logo' src="+streams[i][2]+"><div class='info'><p class='channelname'>"+streams[i][0]+"</p></div></div>");
+  }
+  loadingindicator.style.display = "none";
+}
---
+++ streams.js
@@ -0,0 +1,7 @@
+// name, stream url, logo url
+var streams = [
+  ["Deutschlandfunk", "https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3", "https://pbs.twimg.com/profile_images/857088405745930240/oeraNk9x_400x400.jpg"],
+  ["Deutschlandfunk Nova", "https://st03.sslstream.dlf.de/dlf/03/128/mp3/stream.mp3", "https://pbs.twimg.com/profile_images/885476067493982208/wOB99d6P.jpg"],
+  ["Deutschlandfunk Kultur", "https://st02.sslstream.dlf.de/dlf/02/128/mp3/stream.mp3", "https://pbs.twimg.com/profile_images/857069281988792320/K0mhOJOn.jpg"],
+  ["Bayern 2", "https://br-edge-2007-dus-lg-cdn.cast.addradio.de/br/br2/sued/mp3/128/stream.mp3", "https://i.imgur.com/rS5ER0f.png"]
+];
