8f10e675ac7117404eeeab4c5af32d1db5e3b0c3
nunq <46054695+nunq@users.noreply.github.com>
2024-05-18 16:31:09 +0200

    cleanup dead streams

--- streams.js
+++ streams.js
@@ -3,12 +3,6 @@ var streams = [
   ["Deutschlandfunk", "https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3", "https://i.imgur.com/1LkdICb.jpg"],
   ["Deutschlandfunk Nova", "https://st03.sslstream.dlf.de/dlf/03/128/mp3/stream.mp3", "https://i.imgur.com/aBpooRm.jpg"],
   ["Deutschlandfunk Kultur", "https://st02.sslstream.dlf.de/dlf/02/128/mp3/stream.mp3", "https://i.imgur.com/wKVf4Ws.jpg"],
-  ["Bayern 2", "https://br-edge-2007-dus-lg-cdn.cast.addradio.de/br/br2/sued/mp3/128/stream.mp3", "https://i.imgur.com/rS5ER0f.png"],
-  ["lainon.life cafe", "https://lainon.life/radio/cafe.ogg", "https://i.imgur.com/qQ0QDW4.png"],
-  ["lainon.life cyberia", "https://lainon.life/radio/cyberia.ogg", "https://i.imgur.com/qQ0QDW4.png"],
-  ["lainon.life everything", "https://lainon.life/radio/everything.ogg", "https://i.imgur.com/qQ0QDW4.png"],
-  ["#/g/punk Radio", "http://cyberadio.pw:8000/stream", "https://i.imgur.com/gY5kFNN.png"],
   ["somafm Deep Space One", "https://ice1.somafm.com/deepspaceone-128-mp3", "https://i.imgur.com/CGzSDuR.jpg"],
-  ["somafm GS Classic", "https://ice4.somafm.com/gsclassic-128-mp3", "https://i.imgur.com/MkUQku4.jpg"],
-  ["late.city radio", "http://late.city:8000/latestation.ogg", "https://i.imgur.com/XBfAPMI.png"],
+  ["somafm GS Classic", "https://ice4.somafm.com/gsclassic-128-mp3", "https://i.imgur.com/MkUQku4.jpg"]
 ];
